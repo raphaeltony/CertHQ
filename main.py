6022c28dddbc68c4574c1aabd82f88a73e8da304
Reuben <reubendinny@gmail.com>
2023-02-04 21:25:50 +0530

    file and form upload done

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
  
 app = Flask(__name__)
  
@@ -10,6 +11,22 @@ def index():
 @app.route("/add")
 def addcert():
     return render_template("addcert.html")
+
+@app.route('/uploader', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        print(dict(request.form))
+        f = request.files['file']
+        # e = request.files['event']
+        # i = request.files['instname']
+        if f:
+            f.save(secure_filename(f.filename))
+            # print(e,i)
+            return 'File uploaded successfully'
+        else:
+            return 'Error occured'
+
+
  
 if __name__ == "__main__":
   app.run(debug=True)
